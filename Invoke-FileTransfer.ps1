230afeccb4a59e39fc0c8f411f7c6650802ec646
tylerdotrar <69973771+tylerdotrar@users.noreply.github.com>
2021-05-03 08:31:53 -0400

    v1.0.0

---
+++ Invoke-FileTransfer.ps1
@@ -0,0 +1,325 @@
+﻿function Invoke-FileTransfer {
+#.SYNOPSIS
+# Python x PowerShell user-authentication based file transfer via Flask web server (client-side).
+# ARBITRARY VERSION NUMBER:  1.0.0
+# AUTHOR:  Tyler McCann (@tyler.rar)
+#
+#.DESCRIPTION
+# This script is designed to transfer files (upload and download) to a custom Python-based flask web server, 
+# supporting both HTTP and HTTPS protocols.  The Python web server should only accept files sent from this script 
+# due to the modified HTTP Content-Disposition header name.  On top of that, communications will only work if the
+# user inputs the authorized credentials configured on the web server.  If PowerShell Core is used, file MIME 
+# types are determined based off of a hard-coded list of file extensions; whereas with Desktop Powershell, MIME 
+# types are automatically determined based off of file contents.
+#
+# Alternate data streams (ADS) are NOT supported.
+#
+# Parameters:
+#    -Upload        -->   Upload a file to the web server
+#    -Download      -->   Download a file from the web server
+#    -File          -->   File to upload/download
+#    -URL           -->   URL of the web server
+#    -Help          -->   (Optional) Return Get-Help information
+#    
+# Example Usage:
+#    []  PS C:\Users\Bobby> Invoke-FileTransfer -Upload -File 'Passwords.txt' -URL 'http://192.168.1.24:17540'
+#        [SERVER AUTHENTICATION]
+#        - Username: admin
+#        - Password: password
+#
+#        Server Response (HTTPS): SUCCESSFUL UPLOAD
+#
+#    []  PS C:\Users\Bobby> filetransfer -Download
+#        [TRANSMISSION DATA]
+#        - Filename: CoolPic.png 
+#        - URL: http://192.168.0.69:54321
+#
+#        [SERVER AUTHENTICATION]
+#        - Username: bobby
+#        - Password: schmurda
+#
+#        Server Response (HTTP): SUCCESSFUL DOWNLOAD
+#
+#    []  PS C:\Users\Bobby> Invoke-FileUpload -Upload -File SuperCoolScript.ps1
+#        [TRANSMISSION DATA]
+#        - URL: https://192.168.0.10:12345
+#
+#        [SERVER AUTHENTICATION]
+#        - Username: admin
+#        - Password: notpass
+#
+#        Server Response (HTTPS): UNSUCCESSFUL LOGIN
+#
+#.LINK
+# https://github.com/tylerdotrar/P2.FileTransfer
+
+
+    [Alias('filetransfer')]
+
+    Param (
+        [switch] $Upload,
+        [switch] $Download,
+        [string] $File,
+        [string] $URL = '<url>',
+        [switch] $Help
+    )
+
+    # Internal Functions
+    function HTTPS-Bypass ([switch]$Undo) {
+
+        if ($Undo) { [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $NULL }
+
+        else {
+
+            # [!] Required by non-Core PowerShell for self-signed certificate bypass (HTTPS).
+            $CertBypass = @'
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class WindowsPowerShellCerts
+{
+    public static void Bypass()
+    {
+        ServicePointManager.ServerCertificateValidationCallback = 
+            delegate
+            (
+                Object obj, 
+                X509Certificate certificate, 
+                X509Chain chain, 
+                SslPolicyErrors errors
+            )
+            {
+                return true;
+            };
+    }
+        
+}
+'@
+            Add-Type $CertBypass
+            [WindowsPowerShellCerts]::Bypass()
+        }
+    }
+    function Server-Upload {
+        
+        # Required by Windows PowerShell for HttpClient / HttpClientHandler creation and cert bypass
+        if ($PSEdition -ne 'Core') { Add-Type -AssemblyName System.Net.Http }
+
+        # Bypass HttpClientHandler certificate validation (self-signed certificates)
+        if ($Protocol -eq 'HTTPS') {
+
+            # .NET version independent C# code for bypass
+            $CertBypass = @'
+using System;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SelfSignedCerts
+{
+    public class Bypass
+    {
+         public static Func<HttpRequestMessage,X509Certificate2,X509Chain,SslPolicyErrors,Boolean> ValidationCallback = 
+            (message, cert, chain, errors) => {
+                return true; 
+            };
+    }
+}
+'@
+        
+            if ($PSEdition -eq 'Core') { Add-Type $CertBypass }
+            else { Add-Type $CertBypass -ReferencedAssemblies System.Net.Http }
+        }
+
+
+        # MIME Type Detection (PowerShell Core)
+        if ($PSEdition -eq 'Core') {
+
+            # Extension-based MIME type / Content-Type (Hard-Coded)
+            $MimeTypeMap = @{
+                '.txt'   =  'text/plain';
+                '.jpg'   =  'image/jpeg';
+                '.jpeg'  =  'image/jpeg';
+                '.png'   =  'image/png';
+                '.gif'   =  'image/gif';
+                '.zip'   =  'application/zip';
+                '.rar'   =  'application/x-rar-compressed';
+                '.gzip'  =  'application/x-gzip';
+                '.json'  =  'application/json';
+                '.xml'   =  'application/xml';
+                '.ps1'   =  'application/octet-stream';
+            }
+
+            $Extension = (Get-Item $File).Extension.ToLower()
+            $ContentType = $MimeTypeMap[$Extension]
+        }
+
+
+        # MIME Type Detection (Windows PowerShell)
+        else {
+
+            # Get file MIME type / Content-Type (.NET)
+            Add-Type -AssemblyName System.Web
+            $ContentType = [System.Web.MimeMapping]::GetMimeMapping($File)
+        }
+
+
+        # Create a message handler object for the HttpClient (.NET) and store authorized session cookies
+        $Handler = [System.Net.Http.HttpClientHandler]::new()
+        $Handler.CookieContainer = $SessionID.Cookies
+
+        if ($Protocol -eq 'HTTPS') {
+            $Handler.ServerCertificateCustomValidationCallback = [SelfSignedCerts.Bypass]::ValidationCallback
+        }
+
+
+        # Create an HttpClient object for sending / receiving HTTP(S) data (.NET)
+        $httpClient = [System.Net.Http.HttpClient]::new($Handler)
+
+
+        ## Start Multipart Form Creation ##
+
+        $FileStream = New-Object System.IO.FileStream @($File, [System.IO.FileMode]::Open)
+        $DispositionHeader = New-Object System.Net.Http.Headers.ContentDispositionHeaderValue 'form-data'
+
+        # Custom Content-Disposition header name (Custom Python Server Specific)
+        $DispositionHeader.Name = 'TYLER.RAR'
+        $DispositionHeader.FileName = $TempFileName
+
+        $StreamContent = New-Object System.Net.Http.StreamContent $FileStream
+        $StreamContent.Headers.ContentDisposition = $DispositionHeader
+        $StreamContent.Headers.ContentType = New-Object System.Net.Http.Headers.MediaTypeHeaderValue $ContentType
+        
+        $MultipartContent = New-Object System.Net.Http.MultipartFormDataContent
+        $MultipartContent.Add($StreamContent)
+
+        ## End Multipart Form Creation ##
+
+
+        # Attempt to upload file and return server response
+        Try {
+
+            $Transmit = $httpClient.PostAsync($UploadPage, $MultipartContent).Result
+            $ServerMessage = $Transmit.Content.ReadAsStringAsync().Result
+
+            Write-Host "`nServer Response ($Protocol): " -ForegroundColor Yellow -NoNewline
+            Write-Host $ServerMessage
+        }
+
+
+        # This error will appear if you put in an incorrect URL (or other less obvious things)
+        Catch { 
+            Write-Host 'Failed to reach the server!' -ForegroundColor DarkRed
+            return
+        }
+
+
+        # Cleanup Hanging Processes
+        Finally {
+            if ($NULL -ne $httpClient) { $httpClient.Dispose() }
+            if ($NULL -ne $Transmit) { $Transmit.Dispose() }
+            if ($NULL -ne $FileStream) { $FileStream.Dispose() }
+        }
+    }
+    function Server-Download {
+
+        # Attempt to download file from the server
+        Try {
+
+            if ($PSEdition -ne 'Core') { Invoke-WebRequest $DownloadPage -WebSession $SessionID -Outfile $File }
+            else { Invoke-WebRequest $DownloadPage -SkipCertificateCheck -WebSession $SessionID -Outfile $File }
+
+            Write-Host "`nServer Response ($Protocol): " -NoNewline -ForegroundColor Yellow
+            Write-Host "SUCCESSFUL DOWNLOAD"
+        }
+
+
+        # File doesn't exist on the server
+        Catch {
+            Write-Host "`nServer Response ($Protocol): " -NoNewline -ForegroundColor Yellow
+            Write-Host "FILE NOT FOUND"
+        }
+    }
+    
+
+    # Return Get-Help information
+    if ($Help) { return Get-Help Invoke-FileTransfer }
+
+    # Failed to specify whether to download from or upload to server
+    elseif (!$Download -and !$Upload) { return (Write-Host 'No action specified.' -ForegroundColor Red) }
+
+
+    # Prompt Header
+    if (!$File -or $URL) { Write-Host "[TRANSMISSION DATA]" -ForegroundColor Green } 
+
+
+    # Prompt for File
+    if (!$File) {  Write-Host '- Filename: ' -NoNewLine -ForegroundColor Yellow ; $File = Read-Host }
+
+    # Verify input file (UPLOAD ONLY)
+    if ($Upload) { 
+        
+        if (Test-Path -LiteralPath $File) { $File = (Get-Item -LiteralPath $File).FullName }
+    
+        else {
+            Write-Host 'File does not exist!' -ForegroundColor DarkRed
+            return
+        }
+    }
+    $TempFileName = Split-Path -Leaf $File
+
+
+    # Prompt for Server URL
+    if ($URL -eq '<url>') { Write-Host '- URL: ' -NoNewline -ForegroundColor Yellow ; $URL = Read-Host }
+
+    # Verify protocol
+    if ($URL -like "https://*") { $Protocol = 'HTTPS' }
+    elseif ( $URL -like "http://*") { $Protocol = 'HTTP' }
+    else {
+        Write-Host 'URL neither HTTP nor HTTPS!' -ForegroundColor DarkRed
+        return
+    }
+
+
+    # Server Pages
+    $DownloadPage = $URL + "/en-us/p2ft/download/$TempFileName"
+    $UploadPage   = $URL + "/en-us/p2ft/upload"
+    $LoginPage    = $URL + '/login'
+    $LogoutPage   = $URL + '/logout'
+ 
+    
+    # User Authentication
+    Write-Host "`n[SERVER AUTHENTICATION]" -ForegroundColor Green
+    Write-Host '- Username: ' -NoNewLine -ForegroundColor Yellow ; $ServerUsername = Read-Host
+    Write-Host '- Password: ' -NoNewLine -ForegroundColor Yellow ; $ServerPassword = Read-Host
+
+
+    # Windows PowerShell Self-Signed Certificate Bypass
+    if ($PSEdition -ne 'Core') {
+        HTTPS-Bypass
+        $LoginResponse = (Invoke-WebRequest $LoginPage -Body @{username="$ServerUsername";password="$ServerPassword"} -Method POST -SessionVariable 'SessionID').content
+    }
+    else { $LoginResponse = (Invoke-WebRequest $LoginPage -SkipCertificateCheck -Body @{username="$ServerUsername";password="$ServerPassword"} -Method POST -SessionVariable 'SessionID').content }
+
+
+    if ($LoginResponse -ne 'SUCCESSFUL LOGIN') {
+
+        Write-Host "`nServer Response ($Protocol): " -NoNewline -ForegroundColor Yellow
+        Write-Host $LoginResponse
+
+        return
+    }
+
+    
+    # Main
+    if ($Download)   { Server-Download }
+    elseif ($Upload) { Server-Upload }
+
+    # Logout / Remove Self-Signed Certificate Bypass (Windows PowerShell)
+    if ($PSEdition -eq 'Core') { Invoke-WebRequest $LogoutPage -SkipCertificateCheck -WebSession $SessionID | Out-Null }
+    else {
+        Invoke-WebRequest $LogoutPage -WebSession $SessionID | Out-Null
+        HTTPS-Bypass -Undo
+    }
+}
---
+++ Invoke-HybridServer.ps1
@@ -0,0 +1,338 @@
+﻿function Invoke-HybridServer {
+#.SYNOPSIS
+# Python x PowerShell user-authentication based file transfer via Flask web server (server-side).
+# ARBITRARY VERSION NUMBER:  1.0.0
+# AUTHOR:  Tyler McCann (@tyler.rar)
+#
+#.DESCRIPTION
+# This script is designed to automate server usage -- starting, stopping, user credentials, port, protocol, etc.
+# This server is meant to only communicate with the corresponding file transfer script (Invoke-FileTransfer),
+# ignoring communications via a web browser.  This server will host files to be downloaded from the '/downloads'
+# directory, and post uploaded files to the '/uploads' directory.
+#
+# It creates an entirely new PowerShell window for the server (supporting both Windows PowerShell and PowerShell 
+# Core), allowing you to continue usage of your current terminal.  The script even has Windows Terminal support -- 
+# meaning it detects if the terminal you are using is being used inside of Windows Terminal, and will create a new 
+# tab, start the server, rename the tab to useful server information, and tab back to the previously used Window 
+# (unless -Focus is used). Note: this functionality only works if PowerShell is your default Windows Terminal profile.
+#
+# Parameters:
+#    [Script Configuration]
+#    -Config        -->    Generate new 'hybrid_server.py' in $PSScriptRoot with desired user credentials
+#    -Start         -->    Start the upload server (ONLY works if NO Python instance is open)
+#    -Stop          -->    Stop the upload server (unreliable if more than one Python instance is open)
+#    -Focus         -->    (Optional) Give new server window focus (instead of returning to current terminal)
+#    -Help          -->    (Optional) Return Get-Help information
+#
+#    [Server Configuration]
+#    -SSL           -->    (Optional) Use HTTPS
+#    -IP            -->    (Optional) Change IP address (NOT recommended; only change to 127.0.0.1)
+#    -Port          -->    (Optional) Change port (default is 54321)
+#    -Debug         -->    (Optional) Enable debugger
+#    -Server        -->    (Optional) Absolute path to 'hybrid_server.py'
+#    
+# Example Usage:
+#    []  PS C:\Users\Bobby> Invoke-HybridServer -Config
+#        [SERVER AUTHENTICATION
+#        - Username: admin
+#        - Password: password
+#
+#        'hybrid_server.py' generated.
+#
+#    []  PS C:\Users\Bobby> Hybrid-Server -SSL -Port 4444 -Start
+#        Server started.
+#
+#.LINK
+# https://github.com/tylerdotrar/P2.FileTransfer
+    
+
+    [Alias('Hybrid-Server')]
+
+    Param (
+        # Script
+        [switch] $Config,
+        [switch] $Start,
+        [switch] $Stop,
+        [switch] $Focus,
+        [switch] $Help,
+        
+        # Server
+        [switch] $SSL,
+        [switch] $Debug,
+        [string] $IP,
+        [int]    $Port,
+
+        [string] $Server = "$PSScriptRoot\hybrid_server.py"
+    )
+
+
+    function Server-Generation {
+        
+        function Server-RandomKeyGen ([int]$Length = 20) {
+
+            #Characters and their corresponding ASCII values
+            $Numbers = 48..57
+            $Uppercase = 65..90
+            $Lowercase = 97..122
+            $SpecialCharacters = 60..64 + 91 + 93..94 + 123..125
+            $CharArray = $Numbers + $Uppercase + $Lowercase + $SpecialCharacters
+
+            $SessionKey = $NULL
+            for ($i=0; $i -lt $Length; $i++) {
+                $Num = Get-Random $CharArray
+                $Char = [char]$Num
+                $SessionKey += $Char
+            }
+
+            return $SessionKey
+        }
+
+        Write-Host "[SERVER AUTHENTICATION]" -ForegroundColor Green
+        Write-Host '- Username: ' -NoNewLine -ForegroundColor Yellow ; $ServerUsername = Read-Host
+        Write-Host '- Password: ' -NoNewLine -ForegroundColor Yellow ; $ServerPassword = Read-Host
+
+
+        $SessionKey = Server-RandomKeyGen
+        [string]$ServerConfiguration = @"
+import os, flask, argparse
+from flask import request, send_from_directory, session
+from werkzeug.utils import secure_filename
+from functools import wraps
+
+app = flask.Flask(__name__, static_url_path='')
+
+app.secret_key = '$SessionKey'
+app.config['SERVER_USERNAME'] = '$ServerUsername'
+app.config['SERVER_PASSWORD'] = '$ServerPassword'
+
+app.config['CLIENT_DOWNLOADS'] = './downloads'
+app.config['CLIENT_UPLOADS'] = './uploads'
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg','.png','.pdf','.txt','.zip','.ps1']
+
+
+# REQUIRE LOGIN
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return 'LOGIN REQUIRED'
+    return wrap
+
+
+# FILE DOWNLOAD
+@app.route('/en-us/p2ft/download/<string:file_name>', methods=['GET'])
+@login_required
+def download_file(file_name):
+    try:
+        return send_from_directory(app.config['CLIENT_DOWNLOADS'], filename=file_name, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+
+# FILE UPLOAD
+@app.route('/en-us/p2ft/upload', methods=['GET','POST'])
+@login_required
+def upload_file():
+    if request.method == 'POST':
+        uploaded_file = request.files['TYLER.RAR']
+        file_name = secure_filename(uploaded_file.filename)
+        if file_name != '':
+            file_ext = os.path.splitext(file_name)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                return 'FILETYPE NOT ALLOWED'
+            uploaded_file.save(os.path.join(app.config['CLIENT_UPLOADS'], file_name))
+            return 'SUCCESSFUL UPLOAD'
+        return 'FILENAME NULL'
+    return 'UPLOAD ONLY'
+
+
+# LOGIN PAGE
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST': #LoginPost
+        if request.form['username'] == app.config['SERVER_USERNAME'] and request.form['password'] == app.config['SERVER_PASSWORD']:
+            session['logged_in'] = True
+            return 'SUCCESSFUL LOGIN'
+        else:
+            return 'UNSUCCESSFUL LOGIN'
+    return 'LOGIN ONLY'
+
+
+# LOGOUT PAGE
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return 'SUCCESSFUL LOGOUT'
+
+
+# PARAMETERS
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', default='0.0.0.0')
+    parser.add_argument('--port', default=54321, type=int)
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--ssl', action='store_const', const='adhoc', default=None)
+    args = parser.parse_args()
+
+    app.run(host=args.ip, port=args.port, ssl_context=args.ssl, debug=args.debug)
+"@
+        
+
+        [System.IO.File]::WriteAllLines($Server, $ServerConfiguration)
+        Write-Host "`n'hybrid_server.py' generated." -ForegroundColor Green
+    }
+
+    # Return Get-Help information
+    if ($Help) { return Get-Help Invoke-HybridServer }
+
+    # Create / Update 'hybrid_server.py'
+    elseif ($Config) { Server-Generation ; return }
+
+    # Failed to specify whether to start or stop server
+    elseif (!$Start -and !$Stop) { return (Write-Host 'No action specified.' -ForegroundColor Red) }
+
+
+    # Skip unnecessary code for stopping the server.
+    if (!$Stop) {
+
+        # Server location not properly specified
+        if ( !(Test-Path -LiteralPath $Server) ) { 
+            return (Write-Host 'Server does not exist.' -ForegroundColor Red)
+        }
+
+
+        # Determine if current PowerShell terminal is inside Windows Terminal
+        if ( (((Get-Process -Name 'OpenConsole').StartTime) | ForEach-Object { [string]$_ } ) -Contains [string](Get-Process -ID $PID).StartTime 2>$NULL ) {
+
+            # Create an ordered hashtable of Windows Terminal tabs (Main:PID or Arbitrary:PID)
+            $WindowContext = [ordered]@{}
+            $OpenWindows = (Get-Process -Name 'OpenConsole') | Sort-Object -Property StartTime
+            $Inc = 1
+        
+            foreach ($WinTerm in $OpenWindows) {
+
+                if ( ($WinTerm).StartTime -match (Get-Process -ID $PID).StartTime ) {
+                    $WindowContext += @{'Main' = $WinTerm.ID}
+                }
+                else { $WindowContext += @{"Arbitrary$Inc" = $WinTerm.ID} ; $Inc++ }
+
+            }
+
+
+            # Get Index Number of Current Windows Terminal Session
+            if ($WindowContext.count -eq 1) { $MainWindow = 1 }
+            else { $MainWindow = $($WindowContext.Keys).IndexOf('Main') + 1 }
+
+            $UsingWindowsTerminal = $TRUE
+        }
+
+
+        # Determine PowerShell Version to use for the server
+        else {
+            
+            if ($PSEdition -eq 'Core') { $PowerShell = 'pwsh' }
+            else { $PowerShell = 'powershell' }
+        }
+    
+
+        # Create 'uploads' / 'downloads' directories if they don't already exist
+        $ServerFolder    = $Server.Replace($Server.Split('\')[-1],$NULL)
+
+        $UploadsFolder   = $ServerFolder + 'uploads'
+        $DownloadsFolder = $ServerFolder + 'downloads'
+
+
+        if (!(Test-Path -LiteralPath $UploadsFolder))   { New-Item -ItemType Directory -Path $UploadsFolder   | Out-Null }
+        if (!(Test-Path -LiteralPath $DownloadsFolder)) { New-Item -ItemType Directory -Path $DownloadsFolder | Out-Null }
+
+    
+        # Configure Optional Server Parameters (Windows Terminal)
+        if ($UsingWindowsTerminal) {
+
+            # Windows Terminal server tab title
+            if ($SSL) { $Title = 'HTTPS Server' }
+            else { $Title = 'HTTP Server' }
+
+
+            if ($Port) { $Title += " : $Port" }
+            else { $Title += ' : 54321' }
+
+
+            $Commands = "`$Host.UI.RawUI.WindowTitle = '$Title'; Set-Location -LiteralPath $ServerFolder; Clear-Host; python $Server"
+
+            if ($SSL)   { $Commands += " --ssl" }
+            if ($Debug) { $Commands += " --debug" }
+            if ($IP)    { $Commands += " --ip $IP" }
+            if ($Port)  { $Commands += " --port $Port" }
+
+            $Commands += "; exit"
+        }
+
+        # Configure Optional Server Parameters (Windows PowerShell)
+        else {
+        
+            # Determine server WindowStyle
+            if ($Focus) { $Window = 'Normal' }
+            else { $Window = 'Minimized' }
+
+            $Commands = "-WindowStyle $Window", "-Command Set-Location -LiteralPath $ServerFolder ; python $Server"
+
+            if ($SSL) { $Commands[-1] += " --ssl" }
+            if ($Debug) { $Commands[-1] += " --debug" }
+            if ($IP) { $Commands[-1] += " --ip $IP" }
+            if ($Port) { $Commands[-1] += " --port $Port" }
+        }
+    }
+  
+
+    # Determine if server is already running (NOT ACCURATE)
+    $Running = Get-Process -Name python* 2>$NULL
+
+
+    # Start server
+    if ($Start) {
+
+        if (!$Running) {
+            
+            # Create a new Windows Terminal window
+            if ($UsingWindowsTerminal) {
+                
+                # Initialize Wscript ComObject to send Keystrokes to Applications
+                $WindowsTerminalHack = New-Object -ComObject Wscript.Shell
+
+                # Create new tab (Send CTRL + SHIFT + T to current window)
+                $WindowsTerminalHack.SendKeys('^+t')
+                Start-Sleep -Milliseconds 250
+
+                # Send commands to title the Window and start the server
+                $WindowsTerminalHack.SendKeys("$Commands{ENTER}")
+
+                # Return to original tab (Send CTRL + ALT + Index Number to current window)
+                if (!$Focus) {
+                    $WindowsTerminalHack.SendKeys("^%$MainWindow")
+                }
+            }
+
+            # Open new PowerShell session
+            else { Start-Process -FilePath $PowerShell -ArgumentList $Commands }
+
+            Write-Host 'Server started.' -ForegroundColor Green
+        }
+
+        else { Write-Host 'Server is already running.' -ForegroundColor Green }
+    }
+
+
+    # Stop server
+    elseif ($Stop) {
+
+        if ($Running) {
+            Stop-Process -Name python*
+            Write-Host 'Server stopped.' -ForegroundColor Red
+        }
+
+        else { Write-Host 'Server is already stopped.' -ForegroundColor Red }
+    }
+}
